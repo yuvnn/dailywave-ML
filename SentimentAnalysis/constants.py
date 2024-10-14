5fc8f05c4a8cc03ad0f2c36bde14a5df7d05fb8c
yubin11890 <yubin11890@gmail.com>
2024-10-14 17:13:24 +0900

    train etc

---
+++ SentimentAnalysis/constants.py
@@ -0,0 +1,15 @@
+ID2LABEL_KOR = {0: '기쁨',
+        1: '당황',
+        2: '분노',
+        3: '불안',
+        4: '상처',
+        5: '슬픔',
+        }
+
+ID2LABEL_EN = {0: 'joy',
+                1: 'embarrassed',
+                2: 'anger',
+                3: 'nervous',
+                4: 'hurt',
+                5: 'sad',
+                }
---
+++ SentimentAnalysis/train.py
@@ -0,0 +1,84 @@
+import os
+import constants
+from datasets import load_dataset
+from transformers import AutoModelForSequenceClassification, AutoTokenizer, DataCollatorWithPadding
+from transformers import TrainingArguments, Trainer, logging
+from utils import make_current_datetime_dir, compute_metrics, preprocess_data
+
+
+os.environ["TOKENIZERS_PARALLELISM"] = "false"
+os.environ['TRANSFORMERS_NO_ADVISORY_WARNINGS'] = 'true'
+logging.set_verbosity_error()
+
+
+def train(opt):
+    tokenizer = AutoTokenizer.from_pretrained(opt['pretrained_tokenizer'])
+    id2label = constants.ID2LABEL_EN
+    label2id = {v: k for k, v in id2label.items()}
+    labels = list(label2id.keys())
+    dataset = load_dataset('csv', data_files={'train': opt['train_dataset_path'],
+                                              'val': opt['val_dataset_path']
+                                              }
+                           )
+    dataset = dataset.map(preprocess_data,
+                                  batched=True,
+                                  remove_columns=dataset['train'].column_names,
+                                  fn_kwargs={'tokenizer': tokenizer,
+                                             'labels': labels
+                                             }
+                                  )
+    dataset.set_format('torch')
+    model = AutoModelForSequenceClassification.from_pretrained(opt['pretrained_model'], 
+                                                            problem_type=opt['problem_type'],
+                                                            num_labels=len(labels),
+                                                            id2label=id2label,
+                                                            label2id=label2id)
+    args = TrainingArguments(output_dir=make_current_datetime_dir(opt['output_dir']),
+                            evaluation_strategy=opt['evaluation_strategy'],
+                            save_strategy=opt['save_strategy'],
+                            learning_rate=opt['learning_rate'],
+                            per_device_train_batch_size=opt['per_device_train_batch_size'],
+                            per_device_eval_batch_size=opt['per_device_eval_batch_size'],
+                            num_train_epochs=opt['num_train_epochs'],
+                            weight_decay=opt['weight_decay'],
+                            load_best_model_at_end=opt['load_best_model_at_end'],
+                            metric_for_best_model=opt['metric_for_best_model'],
+                            seed=opt['seed'],
+                            dataloader_num_workers=opt['dataloader_num_workers'],
+                            no_cuda=opt['no_cuda']
+                            )
+    trainer = Trainer(args=args,
+                      model=model,
+                      tokenizer=tokenizer,
+                      train_dataset=dataset['train'],
+                      eval_dataset=dataset['val'],
+                      compute_metrics=compute_metrics,
+                      data_collator=DataCollatorWithPadding(tokenizer=tokenizer)
+                      )
+    
+    trainer.train()
+    
+
+if __name__ == '__main__':
+    opt = {'pretrained_model': 'beomi/KcELECTRA-base-v2022',
+           'pretrained_tokenizer': 'beomi/KcELECTRA-base-v2022',
+           'problem_type': 'multi_label_classification',
+           'train_dataset_path': './data/preprocess/감성대화말뭉치_train.csv', #train data
+           'val_dataset_path': './data/preprocess/감성대화말뭉치_val.csv', # val data
+           'output_dir': 'weights/',
+           'metric_for_best_model': 'f1',
+           'evaluation_strategy': 'epoch',
+           'save_strategy': 'epoch',
+           'seed': 1031,
+           'no_cuda': False,
+           'learning_rate': 5e-6,
+           'per_device_train_batch_size': 16,
+           'per_device_eval_batch_size': 16,
+           'num_train_epochs': 10,
+           'weight_decay': 0.01,
+           'dataloader_num_workers': 4,
+           'load_best_model_at_end': False,
+           }
+    
+    train(opt)
+    print("train complete")
