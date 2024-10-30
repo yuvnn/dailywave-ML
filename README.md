083a601c09bc114b23bdc72bad6c43821ba182ca
yubin11890 <yubin11890@gmail.com>
2024-10-30 21:12:44 +0900

    add DVforEC(5)

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 참고 https://github.com/lkkaram/korean-frown-sentence-classifier
 
-preprocessing
-EDC : EmotionalDialogCorpus (감성대화말뭉치)
+### ./SentimentAnalysis/data/preprocess
+
+EDC : EmotionalDialogCorpus [(감성대화말뭉치)](https://aihub.or.kr/aihubdata/data/view.do?currMenu=&topMenu=&aihubDataSe=data&dataSetSn=86)
+
+DVforEC(5) : Dialogue voice dataset for emotion classification [(감정분류를 위한 대화 음성 데이터셋)](https://aihub.or.kr/aihubdata/data/view.do?currMenu=115&topMenu=100&dataSetSn=263)
 
--- SentimentAnalysis/constants.py
+++ SentimentAnalysis/constants.py
@@ -1,15 +1,35 @@
-ID2LABEL_KOR = {0: '기쁨',
-        1: '당황',
-        2: '분노',
-        3: '불안',
-        4: '상처',
+# #EDC
+# ID2LABEL_KOR = {0: '기쁨',
+#         1: '당황',
+#         2: '분노',
+#         3: '불안',
+#         4: '상처',
+#         5: '슬픔',
+#         }
+
+# ID2LABEL_EN = {0: 'joy',
+#                 1: 'embarrassed',
+#                 2: 'anger',
+#                 3: 'nervous',
+#                 4: 'hurt',
+#                 5: 'sad',
+#                 }
+#------------------------------
+# DVforEC
+ID2LABEL_KOR = {0: '분노',
+        1: '역겨움',
+        2: '공포',
+        3: '행복',
+        4: '중립',
         5: '슬픔',
+        6: '놀람',
         }
 
-ID2LABEL_EN = {0: 'joy',
-                1: 'embarrassed',
-                2: 'anger',
-                3: 'nervous',
-                4: 'hurt',
-                5: 'sad',
+ID2LABEL_EN = {0: 'Angry',
+                1: 'Disgust',
+                2: 'Fear',
+                3: 'Happiness',
+                4: 'Neutral',
+                5: 'Sadness',
+                6: 'Surpise'
                 }
