111f372194ab56c2f6efff48853c48b6475feef1
yubin11890 <yubin11890@gmail.com>
2024-10-31 15:31:00 +0900

    add DVforEC(4_8l)

--- README.md
+++ README.md
@@ -6,3 +6,4 @@ EDC : EmotionalDialogCorpus [(감성대화말뭉치)](https://aihub.or.kr/aihubd
 
 DVforEC(5) : Dialogue voice dataset for emotion classification [(감정분류를 위한 대화 음성 데이터셋)](https://aihub.or.kr/aihubdata/data/view.do?currMenu=115&topMenu=100&dataSetSn=263)
 
+DVforEC(4_8l) :  Dialogue voice dataset for emotion classification (8label)
--- SentimentAnalysis/constants.py
+++ SentimentAnalysis/constants.py
@@ -15,21 +15,42 @@
 #                 5: 'sad',
 #                 }
 #------------------------------
-# DVforEC
+# # DVforEC(5)
+# ID2LABEL_KOR = {0: '분노',
+#         1: '역겨움',
+#         2: '공포',
+#         3: '행복',
+#         4: '중립',
+#         5: '슬픔',
+#         6: '놀람',
+#         }
+
+# ID2LABEL_EN = {0: 'Angry',
+#                 1: 'Disgust',
+#                 2: 'Fear',
+#                 3: 'Happiness',
+#                 4: 'Neutral',
+#                 5: 'Sadness',
+#                 6: 'Surpise'
+#                 }
+#------------------------------
+# DVforEC(4_8l)
 ID2LABEL_KOR = {0: '분노',
-        1: '역겨움',
-        2: '공포',
-        3: '행복',
-        4: '중립',
-        5: '슬픔',
-        6: '놀람',
+        1: '슬픔',
+        2: '불안',
+        3: '상처',
+        4: '당황',
+        5: '기쁨',
+        6: '감사',
+        7: '평온'
         }
 
 ID2LABEL_EN = {0: 'Angry',
-                1: 'Disgust',
+                1: 'Sadness',
                 2: 'Fear',
-                3: 'Happiness',
-                4: 'Neutral',
-                5: 'Sadness',
-                6: 'Surpise'
+                3: 'Hurt',
+                4: 'embarrassed',
+                5: 'Happiness',
+                6: 'Thankful',
+                7 : 'Peaceful'
                 }
