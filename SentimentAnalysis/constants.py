c4e26f3dcbcccbdf9680d02c5661366e6ed8d8e2
yubin11890 <yubin11890@gmail.com>
2024-11-12 14:44:03 +0900

    DVforEC(5_6l)-001 weights

--- SentimentAnalysis/constants.py
+++ SentimentAnalysis/constants.py
@@ -15,7 +15,7 @@
 #                 5: 'sad',
 #                 }
 #------------------------------
-# # DVforEC(5)
+# # DVforEC(5_7l)
 # ID2LABEL_KOR = {0: '분노',
 #         1: '역겨움',
 #         2: '공포',
@@ -31,26 +31,43 @@
 #                 3: 'Happiness',
 #                 4: 'Neutral',
 #                 5: 'Sadness',
-#                 6: 'Surpise'
+#                 6: 'Surprise'
 #                 }
 #------------------------------
-# DVforEC(4_8l)
+# DVforEC(5_6l)
 ID2LABEL_KOR = {0: '분노',
-        1: '슬픔',
-        2: '불안',
-        3: '상처',
-        4: '당황',
-        5: '기쁨',
-        6: '감사',
-        7: '평온'
+        1: '역겨움',
+        2: '공포',
+        3: '행복',
+        4: '슬픔',
+        5: '놀람',
         }
 
 ID2LABEL_EN = {0: 'Angry',
-                1: 'Sadness',
+                1: 'Disgust',
                 2: 'Fear',
-                3: 'Hurt',
-                4: 'embarrassed',
-                5: 'Happiness',
-                6: 'Thankful',
-                7 : 'Peaceful'
+                3: 'Happiness',
+                4: 'Sadness',
+                5: 'Surprise'
                 }
+# #------------------------------
+# # DVforEC(4_8l)
+# ID2LABEL_KOR = {0: '분노',
+#         1: '슬픔',
+#         2: '불안',
+#         3: '상처',
+#         4: '당황',
+#         5: '기쁨',
+#         6: '감사',
+#         7: '평온'
+#         }
+
+# ID2LABEL_EN = {0: 'Angry',
+#                 1: 'Sadness',
+#                 2: 'Fear',
+#                 3: 'Hurt',
+#                 4: 'embarrassed',
+#                 5: 'Happiness',
+#                 6: 'Thankful',
+#                 7 : 'Peaceful'
+#                 }
